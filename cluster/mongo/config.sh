a9af01bdc4b4b9560b0cfafa544b8b908f042efe
yuemenglong <yuemenglong@126.com>
2024-11-15 20:27:27 +0800

    mongo sharding

--- cluster/mongo/config.sh
+++ cluster/mongo/config.sh
@@ -23,6 +23,45 @@ db.adminCommand({ getDefaultRWConcern: 1 })
 
 rs.status()
 
+
+
+
+# sharding的配置
+
+mongod --configsvr --replSet <replicaSetName> --dbpath <dbpath> --bind_ip localhost,<hostname>
+mongosh --host <hostname> --port <port>
+
+rs.initiate(
+  {
+    _id: "configrs",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "172.28.41.59:27017" },
+      { _id : 1, host : "172.28.41.59:27018" },
+      { _id : 2, host : "172.28.41.59:27019" }
+    ]
+  }
+)
+
+mongod --shardsvr --replSet <replSetName> --dbpath <dbpath> --bind_ip localhost,<hostname>
+mongosh --host <hostname> --port <port>
+rs.initiate({
+  _id: "shard1rs",
+  members: [
+      { _id : 0, host : "172.28.41.59:27017" },
+      { _id : 1, host : "172.28.41.59:27018" },
+      { _id : 2, host : "172.28.41.59:27019" }
+  ]
+})
+
+mongos --configdb <configReplSetName>/cfg1.example.net:27019,cfg2.example.net:27019,cfg3.example.net:27019 --bind_ip localhost,<hostname>
+mongosh --host <hostname> --port <port>
+
+sh.addShard("<replSetName>/shard1.example.net:27018,shard2.example.net:27018,shard3.example.net:27018")
+sh.shardCollection("<database>.<collection>", { <shardKey> : "hashed" })
+
+
+
 # db.adminCommand(
 #   {
 #     setDefaultRWConcern : 1,
